8020c4d8f4c9460607fab575bd4bc8c5f26e1d0a
Gaz <garyleeeeee@gmail.com>
2021-03-25 00:58:46 +0800

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by GUOLI LI on 25/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
